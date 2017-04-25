60819bcc9c55626415aa917d183435c062886f1b
mgteutsch <mgt0117@gmail.com>
2017-04-24 19:50:31 -0500

    project setup completed

---
+++ OrientationApi/OrientationApi.Tests/CartOrderTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrientationApi.Tests
+{
+    [TestClass]
+    public class CartOrderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ OrientationApi/OrientationApi.Tests/LineItemTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrientationApi.Tests
+{
+    [TestClass]
+    public class LineItemTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
